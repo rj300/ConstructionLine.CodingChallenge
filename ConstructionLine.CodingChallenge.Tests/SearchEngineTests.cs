19b746da5906801a6d198d1d6af86ffabe1a7b92
Rushi Joshi <rushi.joshi@acresoftware.com>
2021-04-07 23:06:47 +0100

    Add project files.

---
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ConstructionLine.CodingChallenge.Tests
+{
+    [TestFixture]
+    public class SearchEngineTests : SearchEngineTestsBase
+    {
+        [Test]
+        public void ShirtsOnlyTest()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> {Color.Red},
+                Sizes = new List<Size> {Size.Small}
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(results.Shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(results.Shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void PerfomanceTestWithMultiColoredSearchOptions()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Small", Size.Small, Color.Black),
+                new Shirt(Guid.NewGuid(), "Black - Large", Size.Large, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Blue - Medium", Size.Large, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+
+                new Shirt(Guid.NewGuid(), "Yellow - Small", Size.Small, Color.Yellow),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Red, Color.Black, Color.Yellow },
+                Sizes = new List<Size> { Size.Small, Size.Medium },
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(results.Shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(results.Shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void SearchEngineTestWithNoShirtsAvailable()
+        {
+            var shirts = new List<Shirt>
+            {
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Yellow, Color.Red },
+                Sizes = new List<Size> { Size.Small, Size.Large },
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(results.Shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(results.Shirts, searchOptions, results.ColorCounts);
+        }
+    }
+}
---
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public class SearchEngine
+    {
+        private readonly List<Shirt> _shirts;
+
+        public SearchEngine(List<Shirt> shirts)
+        {
+            _shirts = shirts;
+        }
+
+
+        public SearchResults Search(SearchOptions options)
+        {
+            if (options == null || options.Colors == null || options.Sizes == null )
+            {
+                throw new ArgumentException("Options missing");
+            }
+
+            // find the shirts based on the color and size from the options
+            var foundShirts = (from shirt in _shirts.ToArray() 
+                               where (!options.Colors.Any() || options.Colors.Any(coloredShirt => coloredShirt == shirt.Color) )
+                               && (!options.Sizes.Any() || options.Sizes.Any(sizedShirt => sizedShirt == shirt.Size) )
+                               select new { Shirt = shirt, shirt.Size, shirt.Color }).ToList();
+
+            //find all the colored counted shirts from foundShirts 
+            var colorCountWithoutEmpties = (from foundShirt in foundShirts
+                                            group foundShirt by foundShirt.Color
+                                            into coloredByGroup
+                                            select new ColorCount
+                                            { Color = coloredByGroup.Key, Count = coloredByGroup.Count() })
+                                            .ToList();
+            var sizedCountResultWithoutEmpties = (from foundShirt in foundShirts
+                                                  group foundShirt by foundShirt.Size
+                                                  into sizedGroup
+                                                  select new SizeCount
+                                                  { Size = sizedGroup.Key, Count = sizedGroup.Count() })
+                                                  .ToList();
+
+            var coloredResult = ColorResultsWithEmpty(colorCountWithoutEmpties);
+            var sizedResult = SizedResultsWithEmpty(sizedCountResultWithoutEmpties);
+
+            return new SearchResults
+            {
+                Shirts = foundShirts.Select(x => x.Shirt).ToList(),
+                ColorCounts = coloredResult,
+                SizeCounts = sizedResult
+            };
+        }
+
+        private List<ColorCount> ColorResultsWithEmpty(List<ColorCount> coloredList)
+        {
+            foreach (var missedColor in Color.All.Where(color => !coloredList.Any(x => x.Color == color)))
+            {
+                coloredList.Add(new ColorCount { Color = missedColor, Count = 0 });
+            }
+
+            return coloredList;
+        }
+
+        private List<SizeCount> SizedResultsWithEmpty(List<SizeCount> sizedList)
+        {
+            foreach(var missedSize in Size.All.Where(size => !sizedList.Any(x=>x.Size == size)))
+            {
+                sizedList.Add(new SizeCount { Size = missedSize, Count = 0 });
+            }
+
+            return sizedList;
+        }
+    }
+}
